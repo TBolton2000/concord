509425dbfde4c5cc42dac1dcdaf11d115da12172
Ubuntu <ubuntu@ip-172-31-16-102.ec2.internal>
2020-10-15 21:36:36 +0000

    Login Page - Qusai

--- src/client/components/Login.tsx
+++ src/client/components/Login.tsx
@@ -1,5 +1,8 @@
 import React, { useState } from "react";
-import { Card, TextField, Button, CardContent } from "@material-ui/core";
+import { Card, TextField, Button, CardContent, Paper, Tab, Tabs, Link, CardHeader } from "@material-ui/core";
+
+import { RouterExample } from "./RouterExample";
+import { SignUp } from "./Signup";
 
 const onTextFieldUpdate = (toUpdate : React.Dispatch<React.SetStateAction<string>>) => {
     return (e : React.ChangeEvent<HTMLInputElement>) => {
@@ -23,16 +26,36 @@ export const Login: React.FC<LoginProps> = () => {
     }
 
     return(
+        
+
         <div>
-            <h3>Login</h3>
-            <Card>
-                <CardContent>
-                    <TextField label="User Name" onChange={onTextFieldUpdate(setUserName)}></TextField>
-                    <Button onClick={()=>{printData(userName, password)}}>
-                        Test
-                    </Button>
-                </CardContent>
-            </Card>
+
+            <Paper square>
+                    <Card>
+                            <CardHeader title='Welcome Back!' />
+                            <CardContent>
+                                <Tabs
+                                    value={userName}
+                                    indicatorColor="primary"
+                                    textColor="primary"
+                                    onChange={onTextFieldUpdate(setUserName)}>
+                                    <Tab label="Login" />
+                                    <Link href='/signup'><Tab label="Sign Up" /></Link>
+                                </Tabs>
+                            </CardContent>
+                            <CardContent><TextField label="User Name" onChange={onTextFieldUpdate(setUserName)}></TextField></CardContent>
+                            <CardContent><TextField label="Password" onChange={onTextFieldUpdate(setPassword)}></TextField></CardContent>
+                            <CardContent>
+                                <Button onClick={()=>{printData(userName, password)}}>
+                                Submit
+                                </Button>
+                            </CardContent>
+                            {/* <CardContent> "Forgot password?" component={Link} to={/components/} </CardContent> */}
+                            <CardContent > <Link href='/'> Forgot password?</Link> </CardContent>
+
+                    </Card>
+            </Paper>
+
         </div>
     );
 }
