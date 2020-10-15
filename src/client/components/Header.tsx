7cb1bf861874d5a3955c6d6d4228731347017df7
peiman95 <peiman.mohseni@tamu.com>
2020-10-15 18:57:17 -0500

    header1

--- src/client/components/Header.tsx
+++ src/client/components/Header.tsx
@@ -1,23 +1,55 @@
-import { AppBar, makeStyles, Toolbar, Typography } from '@material-ui/core';
+import { AppBar, makeStyles, Toolbar, Typography, Button } from '@material-ui/core';
 import { createStyles, Theme } from '@material-ui/core/styles';
 import React from 'react';
+import SettingsIcon from '@material-ui/icons/Settings';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     appBar: {
       zIndex: theme.zIndex.drawer + 1,
     },
+    root: {
+      flexGrow: 1,
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+    },
+    title: {
+      flexGrow: 1,
+    },
   }),
 );
-export const Header: React.FunctionComponent = () => {
+
+
+
+export const Header: React.FunctionComponent = ({isloggedin, username}) => {
+  isloggedin = true
   const classes = useStyles({});
+  if (!isloggedin) {
+    return (
+      <AppBar position='fixed' className={classes.appBar}>
+        <Toolbar>
+          <Typography variant="h6" className={classes.title}>
+            Concord
+          </Typography>
+          <Button color="inherit">Login</Button>
+          <Button color="inherit">Sign up</Button>
+        </Toolbar>
+      </AppBar>
+    );
+  }
+  
   return (
     <AppBar position='fixed' className={classes.appBar}>
       <Toolbar>
-        <Typography variant='h6' noWrap>
-          Fullstack TypeScript
+        <Typography variant="h6" className={classes.title}>
+          Concord
         </Typography>
+        <Button color="inherit"><SettingsIcon color="inherit"/></Button>
+        <Button color="inherit">username</Button>
       </Toolbar>
     </AppBar>
   );
+
 };
+
