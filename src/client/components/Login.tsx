e8414341ecfcffee7186e89a3a68dd19097cfe11
Ubuntu <ubuntu@ip-172-31-16-102.ec2.internal>
2020-10-16 03:05:48 +0000

    updated login page

--- src/client/components/Login.tsx
+++ src/client/components/Login.tsx
@@ -1,9 +1,6 @@
 import React, { useState } from "react";
 import { Card, TextField, Button, CardContent, Paper, Tab, Tabs, Link, CardHeader } from "@material-ui/core";
 
-import { RouterExample } from "./RouterExample";
-import { SignUp } from "./Signup";
-
 const onTextFieldUpdate = (toUpdate : React.Dispatch<React.SetStateAction<string>>) => {
     return (e : React.ChangeEvent<HTMLInputElement>) => {
         toUpdate(e.target.value);
@@ -24,38 +21,26 @@ export const Login: React.FC<LoginProps> = () => {
             console.log(arg);
         }
     }
-
     return(
-        
-
         <div>
 
             <Paper square>
-                    <Card>
-                            <CardHeader title='Welcome Back!' />
-                            <CardContent>
-                                <Tabs
-                                    value={userName}
-                                    indicatorColor="primary"
-                                    textColor="primary"
-                                    onChange={onTextFieldUpdate(setUserName)}>
-                                    <Tab label="Login" />
-                                    <Link href='/signup'><Tab label="Sign Up" /></Link>
-                                </Tabs>
-                            </CardContent>
-                            <CardContent><TextField label="User Name" onChange={onTextFieldUpdate(setUserName)}></TextField></CardContent>
-                            <CardContent><TextField label="Password" onChange={onTextFieldUpdate(setPassword)}></TextField></CardContent>
-                            <CardContent>
-                                <Button onClick={()=>{printData(userName, password)}}>
-                                Submit
-                                </Button>
-                            </CardContent>
-                            {/* <CardContent> "Forgot password?" component={Link} to={/components/} </CardContent> */}
-                            <CardContent > <Link href='/'> Forgot password?</Link> </CardContent>
-
-                    </Card>
+                <Card>
+                    <CardHeader title='Login Page' />
+                        <CardContent>
+                            <Tabs
+                                indicatorColor="primary"
+                                textColor="primary">
+                                <Tab label="Login" />
+                                <Link href='/signup'><Tab label="Sign Up" /></Link>
+                            </Tabs>
+                            <TextField label="User Name"></TextField>
+                            <TextField label="Password"></TextField>
+                            <Button onClick={()=>{printData(userName, password)}}>Submit</Button>
+                            <Link href='/'> Forgot password?</Link> 
+                        </CardContent>
+                </Card>
             </Paper>
-
         </div>
     );
 }
