b0963decb163f744a973496732a2062174c5f31a
EC2 Default User <ec2-user@ip-172-31-22-152.ec2.internal>
2020-10-16 16:17:39 +0000

    Signup page revised part 2

--- src/client/components/Signup.tsx
+++ src/client/components/Signup.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Card, TextField, Button, CardContent, Paper, Link, Tabs, CardHeader, Tab } from "@material-ui/core";
+import { Card, TextField, Button, CardContent, Paper, Link, Tabs, Tab } from "@material-ui/core";
 
 const onTextFieldUpdate = (toUpdate : React.Dispatch<React.SetStateAction<string>>) => {
     return (e : React.ChangeEvent<HTMLInputElement>) => {
@@ -26,12 +26,11 @@ export const SignUp: React.FC<SignUpProps> = () => {
 
     const properEmail = () => {
         const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        console.log(re.test(String(email).toLowerCase()));
         return re.test(String(email).toLowerCase()) || email === "";
     }
 
     const properPassword = () => {
-        return !((password.length < 9 || password.length > 255) && password.length > 0); 
+        return ((password.length > 9 && password.length < 255) || password.length == 0); 
     }
     
 
@@ -53,10 +52,10 @@ export const SignUp: React.FC<SignUpProps> = () => {
                         </Tabs>
                         <TextField label="Name" onChange={onTextFieldUpdate(setName)}></TextField>
                         <TextField label="Username" onChange={onTextFieldUpdate(setUserName)}></TextField>
-                        <TextField error={!properEmail()} label="Email" onChange={onTextFieldUpdate(setEmail)} helperText={!properEmail() ?  'Email is Invalid' : ''}></TextField>
-                        <TextField error = {!areSame(confirmedEmail, email)} label="Confirm Email" onChange={onTextFieldUpdate(setConfirmedEmail)} helperText={!areSame(confirmedEmail, email) ? 'Emails do not match' : ''}></TextField>
-                        <TextField error = {!properPassword()} label="Password" onChange={onTextFieldUpdate(setPassword)} helperText={!properPassword() ? 'Password must be between 9 and 255 characters' : ''}></TextField>
-                        <TextField error = {!areSame(confirmedPassword, password)} label="Confirm Password" onChange={onTextFieldUpdate(setConfirmedPassword)} helperText={!areSame(confirmedPassword, password) ? 'Passwords do not match' : ''}></TextField>
+                        <TextField label="Email" onChange={onTextFieldUpdate(setEmail)} helperText={!properEmail() ?  'Email is Invalid' : ''} error={!properEmail()}></TextField>
+                        <TextField label="Confirm Email" onChange={onTextFieldUpdate(setConfirmedEmail)} helperText={!areSame(confirmedEmail, email) ? 'Emails do not match' : ''} error={!areSame(confirmedEmail, email)}></TextField>
+                        <TextField label="Password" onChange={onTextFieldUpdate(setPassword)} helperText={!properPassword() ? 'Password must be between 9 and 255 characters' : ''} error={!properPassword()} type="password"></TextField>
+                        <TextField label="Confirm Password" onChange={onTextFieldUpdate(setConfirmedPassword)} helperText={!areSame(confirmedPassword, password) ? 'Passwords do not match' : ''} error={!areSame(confirmedPassword, password)} type="password"></TextField>
                         <Button onClick={()=>{printData(name, userName, email, password)}}>
                             Sign Up
                         </Button>
