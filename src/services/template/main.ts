f293e87a7e0433bd6a79bfa32ea09a517a619b2f
TBolton2000 <TBolton2000@aol.com>
2020-10-28 19:40:44 -0500

    clean up

--- src/services/template/main.ts
+++ src/services/template/main.ts
@@ -26,34 +26,7 @@ console.log(`*******************************************`);
     app.set('view engine', 'ejs');
 
     app.use(cookieParser());
-    app.post("/refresh_token", async (req, res) => {
-        const token = req.cookies.jid;
-        if (!token) {
-            return res.send({ ok: false, accessToken: ""});
-        }
-
-        let payload = null;
-        try {
-            payload = verify(token, process.env.REFRESH_TOKEN_SECRET);
-        } catch (error) {
-            console.log(error);
-            return res.send({ ok: false, accessToken: ""});
-        }
-        const user = await User.findOne({id: payload.userId});
-
-        if (!user) {
-            return res.send({ ok: false, accessToken: ""});
-        }
-
-        if (user.tokenVersion !== payload.tokenVersion) {
-            return res.send({ ok: false, accessToken: "" });
-        }
-
-        sendRefreshToken(res, createRefreshToken(user));
-
-        return res.send({ ok: true, accessToken: createAccessToken(user)});
-    })
-
+    
     await createConnection();
 
     const apolloServer = new ApolloServer({
--- src/services/template/routes/api-router.ts
+++ src/services/template/routes/api-router.ts
@@ -1,23 +1,40 @@
 import bodyParser from 'body-parser';
 import { Router } from 'express';
-import { users, getUserById } from '../db';
+import { verify } from 'jsonwebtoken';
+import { createAccessToken, createRefreshToken } from '../auth';
+import { User } from '../entity/User';
+import { sendRefreshToken } from '../sendRefreshToken';
 
 export function apiRouter() {
   const router = Router();
-  router.use(bodyParser.json());
 
-  router.get('/api/users', (req, res) => {
-    res.json(users);
-  });
+  router.post("/refresh_token", async (req, res) => {
+    const token = req.cookies.jid;
+    if (!token) {
+        return res.send({ ok: false, accessToken: ""});
+    }
 
-  router.get('/api/user/:userId', (req, res) => {
-    const userId = req.params.userId;
-    res.json(getUserById(userId));
-  });
+    let payload = null;
+    try {
+        payload = verify(token, process.env.REFRESH_TOKEN_SECRET);
+    } catch (error) {
+        console.log(error);
+        return res.send({ ok: false, accessToken: ""});
+    }
+    const user = await User.findOne({id: payload.userId});
 
-  router.post('/api/set-user', (req, res) => {
-    res.send(`ok`);
-  });
+    if (!user) {
+        return res.send({ ok: false, accessToken: ""});
+    }
+
+    if (user.tokenVersion !== payload.tokenVersion) {
+        return res.send({ ok: false, accessToken: "" });
+    }
+
+    sendRefreshToken(res, createRefreshToken(user));
+
+    return res.send({ ok: true, accessToken: createAccessToken(user)});
+})
 
   return router;
 }
