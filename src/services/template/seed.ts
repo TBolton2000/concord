d95dda1f538d9b4d582c55529843134559bb4e5d
Mahmood Shilleh <AbuZAIN@Mahmoods-MacBook-Pro.local>
2020-11-14 23:51:57 -0600

    Cleaning up to make Trevor happy

--- src/services/template/seed.ts
+++ src/services/template/seed.ts
@@ -1,70 +1,19 @@
 import "reflect-metadata"
 import { createConnection } from 'typeorm';
 import { User } from './entity/User';
+import { UserData } from '../../shared/Users.seed'
 
 createConnection().then(async (connection) => {
     const userRepository = connection.getRepository(User);
 
-    const userOne = new User();
-    userOne.name = 'Mahmood Shilleh';
-    userOne.email = 'Shilleh@gmail.com';
-    userOne.password = 'Shilleh@gmail.com';
-    userRepository.save(userOne);
-
-    const userTwo = new User();
-    userTwo.name = 'Trever Bolton'
-    userTwo.email = 'Bolton@gmail.com';
-    userTwo.password = 'Bolton@gmail.com';
-    userRepository.save(userTwo);
-
-    const userThree = new User();
-    userThree.name = 'Qusai Amer';
-    userThree.email = 'Qusai@gmail.com';
-    userThree.password = 'Qusai@gmail.com'
-    userRepository.save(userThree);
-
-    const userFour = new User();
-    userFour.name = 'Kyle Albin';
-    userFour.email = 'Albin@gmail.com';
-    userFour.password = 'Albin@gmail.com';
-    userRepository.save(userFour);
-
-    const userFive = new User();
-    userFive.name = 'Hank Walker';
-    userFive.email = 'Walker@gmail.com';
-    userFive.password = 'Professor@gmail.com'
-    userRepository.save(userFive);
-
-    const userSix = new User();
-    userSix.name = 'Faris Shatat';
-    userSix.email = 'Shatat@gmail.com';
-    userSix.password = 'Shatat@gmail.com';
-    userRepository.save(userSix);
-
-    const userSeven = new User();
-    userSeven.name = 'Shilleh Bean';
-    userSeven.email = 'ShillehBean@gmail.com';
-    userSeven.password = 'ShillehBean@gmail.com'
-    userRepository.save(userSeven);
-
-    const userEight = new User();
-    userEight.name = 'Chef Shilleh';
-    userEight.email = 'ShefShilleh@gmail.com';
-    userEight.password = 'ShefShilleh@gmail.com'
-    userRepository.save(userEight);
-
-    const userNine = new User();
-    userNine.name = 'CoachShelleh';
-    userNine.email = 'CoachShelleh@gmail.com';
-    userNine.password = 'CoachShelleh@gmail.com';
-    userRepository.save(userNine);
+    for (let userData of UserData) {
+        const user = new User();
+        user.name = userData.name;
+        user.email = userData.email;
+        user.password = userData.password;
+        userRepository.save(user);
+    }
     
-    const userTen = new User();
-    userTen.name = 'Shmileh';
-    userTen.email = 'Shmileh@gmail.com';
-    userTen.password = 'Shmileh@gmail.com';
-    userRepository.save(userTen);
-
 
 }).catch((error) => console.log(error));
 
