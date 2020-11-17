e16c872ac22f06ac1e032bcbce815db44516519b
Mahmood Shilleh <AbuZAIN@Mahmoods-MacBook-Pro.local>
2020-11-16 22:43:16 -0600

    modified seed.ts as i mentioned was changed after i fetched

--- src/services/template/seed.ts
+++ src/services/template/seed.ts
@@ -1,19 +1,29 @@
 import "reflect-metadata"
 import { createConnection } from 'typeorm';
 import { User } from './entity/User';
+import { Event } from './entity/Event';
 import { UserData } from '../../shared/Users.seed'
+import { EventData } from '../../shared/Events.seed'
 import { hash } from "bcryptjs";
 
 createConnection().then(async (connection) => {
     const userRepository = connection.getRepository(User);
-
     for (let userData of UserData) {
         const user = new User();
         user.name = userData.name;
         user.email = userData.email;
         user.password = await hash(userData.password, 12);
-        userRepository.save(user);
+        await userRepository.save(user);
     }
-    
+    const eventRepository = connection.getRepository(Event);
+    for (let eventData of EventData) {
+        const event = new Event();
+        event.title = eventData.title;
+        event.time = eventData.time;
+        event.description = eventData.description;
+        event.videoChatLink = eventData.videoChatLink;
+        await eventRepository.save(event);
+    }
+    process.exit(0);
 }).catch((error) => console.log(error));
 
