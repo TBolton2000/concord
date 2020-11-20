19aeaf978fdf897e8e2d33f96d37e1bfef059e0a
peiman95 <peiman.mohseni@tamu.com>
2020-11-19 18:04:55 -0600

    Text Editor is completed

--- src/client/components/CodeEditor.tsx
+++ src/client/components/CodeEditor.tsx
@@ -1,33 +1,47 @@
-import { Typography, Button, Avatar, Container } from '@material-ui/core';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
-import CardHeader from '@material-ui/core/CardHeader';
-import React from 'react';
-import styled from 'styled-components';
-import 'golden-layout/src/css/goldenlayout-base.css';
-import 'golden-layout/src/css/goldenlayout-dark-theme.css';
-import GoldenLayout, { Row, Stack, createGoldenLayoutComponent } from 'react-golden-layout';
+import React, { useState } from "react";
+import styled from "styled-components";
+import { MarkedInput } from "./MarkedInput";
+import { Result } from "./Result";
+import EditorContext from "./EditorContext";
 
+const AppContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
 
-export const CodeEditor: React.FunctionComponent = () => {
-    return(
-        <GoldenLayout>
-            <Row>
-                <Stack>
-                    {
-                        // To apply properties to a React component, use createGoldenLayoutComponent
-                    }
-                    {createGoldenLayoutComponent(<Button/>, { isClosable: false, title: "Foo's Title" })}
-                </Stack>
-                <Stack>
-                    {
-                        // If you don't want any special settings you can just pass
-                        // React components directly.
-                    }
-                    {<Button/>}
-                </Stack>
-            </Row>
-        </GoldenLayout>
-    );
-    
-};
+const Title = styled.div`
+  font-size: 25px;
+  font-weight: 700;
+  font-family: "Lato", sans-serif;
+  margin-bottom: 1em;
+`;
+
+const EditorContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+`;
+
+export const CodeEditor: React.FunctionComponent = () =>{
+  const [markdownText, setMarkdownText] = useState("");
+
+  const contextValue = {
+    markdownText,
+    setMarkdownText
+  };
+
+  return (
+    <EditorContext.Provider value={contextValue}>
+      <AppContainer>
+        <Title>Markdown Editor</Title>
+        <EditorContainer>
+          <MarkedInput />
+          <Result />
+        </EditorContainer>
+      </AppContainer>
+    </EditorContext.Provider>
+  );
+}
